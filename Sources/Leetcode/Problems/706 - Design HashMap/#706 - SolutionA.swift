ea049f3678c07938eaf20e49a78c6af07ac94f79
Mikhail Dudarev <dev@mikhaildudarev.com>
2022-05-09 15:16:12 +0300

    refactor solution to use simple HashMap from Algo package

--- Sources/Leetcode/Problems/706 - Design HashMap/#706 - SolutionA.swift
+++ Sources/Leetcode/Problems/706 - Design HashMap/#706 - SolutionA.swift
@@ -1,54 +1,27 @@
 //  Created by Mikhail Dudarev on 27.04.2022.
 
+import Algo
+
 extension Problem706DesignHashMap {
     
     final class SolutionA: Problem706DesignHashMapSolution {
         
-        // MARK: - Type Aliases
-        typealias BucketElement = (key: Int, value: Int)
+        private let hashMap: HashMap.Naive<Int, Int>
         
-        // MARK: - Properties
-        let capacity: Int = 10000
-        var buckets: [[BucketElement]]
-        
-        // MARK: - Init/Deinit
         init() {
-            buckets = Array(repeating: [], count: capacity)
+            hashMap = HashMap.Naive<Int, Int>(capacity: 10000)
         }
         
-        // MARK: - Private Methods
-        private func bucketIndex(for key: Int) -> Int {
-            key % capacity
-        }
-        
-        // MARK: - Internal Methods
         func put(_ key: Int, _ value: Int) {
-            let newElement = BucketElement(key: key, value: value)
-            let bucketIndex = bucketIndex(for: key)
-            let elementIndex = buckets[bucketIndex].firstIndex { $0.key == key}
-            guard let elementIndex = elementIndex else {
-                buckets[bucketIndex].append(newElement)
-                return
-            }
-            buckets[bucketIndex][elementIndex] = newElement
+            hashMap.set(value, forKey: key)
         }
         
         func get(_ key: Int) -> Int {
-            let bucketIndex = bucketIndex(for: key)
-            let element = buckets[bucketIndex].first { $0.key == key }
-            guard let element = element else {
-                return -1
-            }
-            return element.value
+            hashMap.get(forKey: key) ?? -1
         }
         
         func remove(_ key: Int) {
-            let bucketIndex = bucketIndex(for: key)
-            let elementIndex = buckets[bucketIndex].firstIndex { $0.key == key }
-            guard let elementIndex = elementIndex else {
-                return
-            }
-            buckets[bucketIndex].remove(at: elementIndex)
+            hashMap.remove(forKey: key)
         }
         
     }
