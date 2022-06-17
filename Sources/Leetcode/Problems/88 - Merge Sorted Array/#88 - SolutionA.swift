26d6dc9a54f46fbe6a1c4df7173e4171a217bd1d
Mikhail Dudarev <dev@mikhaildudarev.com>
2022-06-17 10:42:29 +0300

    refactor SolutionA for problem #88

--- Sources/Leetcode/Problems/88 - Merge Sorted Array/#88 - SolutionA.swift
+++ Sources/Leetcode/Problems/88 - Merge Sorted Array/#88 - SolutionA.swift
@@ -10,22 +10,12 @@ extension Problem88MergeSortedArray {
             var index1 = nums1Slice.startIndex
             var index2 = nums2.startIndex
             for index in (nums1.startIndex..<nums1.endIndex) {
-                if index1 >= nums1Slice.endIndex {
+                if index1 >= nums1Slice.endIndex || (index2 < nums2.endIndex && nums1Slice[index1] >= nums2[index2]) {
                     nums1[index] = nums2[index2]
                     index2 += 1
-                    continue
-                }
-                if index2 >= nums2.endIndex {
-                    nums1[index] = nums1Slice[index1]
-                    index1 += 1
-                    continue
-                }
-                if nums1Slice[index1] < nums2[index2] {
-                    nums1[index] = nums1Slice[index1]
-                    index1 += 1
                 } else {
-                    nums1[index] = nums2[index2]
-                    index2 += 1
+                    nums1[index] = nums1Slice[index1]
+                    index1 += 1
                 }
             }
         }
